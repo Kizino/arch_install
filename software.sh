31e95c3e8ba5c15844fccd6e883f1f78d1f98460
Thanh <thanhnguyen1206@gmail.com>
2022-08-08 01:14:05 -0500

    Added cron

--- software.sh
+++ software.sh
@@ -12,9 +12,13 @@ PKGS=(
         'bluez-utils'           # Bluetooth development and debugging utilities
         'bluez-firmware'        # Firmwares for Broadcom BCM203x and STLC2300 Bluetooth chips
         'pipewire-pulse'        # Bluetooth support for PipeWire
+        'cronie'                # Cron job
 )
 
 for PKG in "${PKGS[@]}"; do
     echo "INSTALLING ${PKG}"
     sudo pacman -S "$PKG" --noconfirm --needed
 done
+
+sudo systemctl enable cronie.service
+sudo systemctl start cronie.service
